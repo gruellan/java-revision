d3c93cf8d8319f7932e47b9dee4a1c7a734c0639
George Ruellan <g.ruellan@lancaster.ac.uk>
2019-05-31 16:18:45 +0100

    Initial commit with introduction programs

---
+++ Introduction/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i=0; i <200; i++) {
+            System.out.println("For loops are fun.");
+        }
+        int i = 200;
+        while(i>0) {
+            System.out.println("While loops are fun also.");
+            i--;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# java-revision
+# Java Revision
+
+Here are some short programs covered in the SCC.110 course. <br>
+Feel free to use these for revision or revise by contributing more programs! :)
